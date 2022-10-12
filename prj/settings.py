2cc9256ccdd045cadc83de02e425e59563a7be8c
masa <ayaayasaesae@gmail.com>
2022-10-12 19:38:15 +0900

    4th commit

--- prj/settings.py
+++ prj/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'django-insecure-a6kuw3fsc*&h_+ykuxnqvnlna*ap@r5_u!8z^uvw0ed3yoaqdq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '54.249.138.119', 'ec2-54-249-138-119.ap-northeast-1.compute.amazonaws.com']
 
